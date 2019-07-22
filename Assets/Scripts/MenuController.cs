488a1e25304826aa8525ce9380c0e026155cc26b
1163200112 <36834933+1163200112@users.noreply.github.com>
2019-07-22 10:58:56 +0800

    v0.1
    v0.1
    只有三关，功能简单

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+/*
+ * Small behaviour to handle menu button callbacks.
+ */
+public class MenuController : MonoBehaviour
+{
+  /*
+   * When the start button is pressed, load the Game scene.
+   */
+  public void OnStartClicked()
+  {
+    Application.LoadLevel("Level0");
+  }
+
+    public void OnstartClicked1()
+    {
+        Application.LoadLevel("Level1");
+    }
+
+    public void OnstartClicked2()
+    {
+        Application.LoadLevel("Level2");
+    }
+
+    public void OnQuitClicked()
+    {
+        Application.Quit();
+    }
+
+    /*
+     * When the back button is clicked, load the Menu scene.
+     */
+    public void OnBackClicked()
+  {
+    Application.LoadLevel("Menu");
+  }
+}
